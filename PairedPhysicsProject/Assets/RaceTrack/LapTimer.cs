c09bb4703e6c520dbb07c903d24858b79a04a797
DogRoss <edgaragunther@gmail.com>
2022-09-13 10:52:03 -0700

    race track -ed

---
+++ PairedPhysicsProject/Assets/RaceTrack/LapTimer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapTimer : MonoBehaviour
+{
+    public static LapTimer instance;
+    public List<CheckPoint> checkPoints;
+
+    public bool counting = false;
+    public float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counting)
+            currentTime += Time.deltaTime;
+    }
+
+    public void StartTimer()
+    {
+        counting = true;
+    }
+
+    public void PassCheckPoint(CheckPoint checkPoint)
+    {
+        foreach(CheckPoint c in checkPoints)
+        {
+            if(c == checkPoint)
+            {
+                c.passed = true;
+            }
+        }
+    }
+
+    public void CheckWinCondition()
+    {
+        bool win = true;
+        foreach(CheckPoint c in checkPoints)
+        {
+            if (!c.passed)
+                win = false;
+        }
+
+        if (win)
+        {
+            counting = false;
+
+            //bring up menu and freeze player movement here
+        }
+    }
+}
---
+++ PairedPhysicsProject/Assets/RaceTrack/NitrousPickup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NitrousPickup : MonoBehaviour
+{
+    public float NitrousAmount = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("enter trigger");
+
+        //if player, then take player and give nitrous
+        //if (other.TryGetComponent<RacingContro>)
+        {
+
+        }
+    }
+}
